96f6cda12c1c7067ef0d6c9a21c4d69e101a7474
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2020-03-06 15:11:51 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(name='soundification',
+      version='1.0.0',
+      description='Multi-Channel Audio Classifier',
+      long_description=read_md('README.md'),
+      author='Ravi Singh',
+      author_email='mailbox.ravisingh@gmail.com',
+      url='https://github.com/ravising-h/soundification',
+      download_url='https://github.com/ravising-h/soundification/tarball/1.0.0',
+      license='MIT',
+      install_requires=['pytorch', 'librosa', 'sys','py3nvml'],
+      packages=find_packages()
+) 
